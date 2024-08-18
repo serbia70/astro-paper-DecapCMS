e2c3bb898b9cce0d0fabddd677cbaebc7a1a555d
github <hao999999@gmail.com>
2024-08-18 15:47:59 +0200

    3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import vercel from "@astrojs/vercel/serverless";
 import decapCmsOauth from "astro-decap-cms-oauth";
 import { defineConfig } from "astro/config";
-import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import remarkToc from "remark-toc";
